d0f4105791f41da66878699d1f6b68206c4c2cca
Luke 10X <spam@luke10x.dev>
2023-04-09 16:27:51 -0400

    wall can be drawn ok with clipping

--- demo.c
+++ demo.c
@@ -91,8 +91,8 @@ void movePlayer() {
   if (K.s == 1 && K.m == 0) { P.x-=dx; P.y-=dy; printf("back\n"); }
 
   // strafe left, right
-  if (K.sl == 1) { P.x+=dy; P.y-=dx; printf("strafe left\n"); }
-  if (K.sr == 1) { P.x-=dy; P.y+=dx; printf("strafe right\n"); } 
+  if (K.sr == 1) { P.x+=dy; P.y-=dx; printf("strafe left\n"); }
+  if (K.sl == 1) { P.x-=dy; P.y+=dx; printf("strafe right\n"); } 
   // move up, down, look up, look down
   if (K.a == 1 && K.m == 1) { P.l-=1; printf("look up\n"); }
   if (K.d == 1 && K.m == 1) { P.l+=1; printf("look down\n"); }
@@ -100,6 +100,56 @@ void movePlayer() {
   if (K.s == 1 && K.m == 1) { P.z+=4; printf("move down\n"); }
 }
 
+void clipBehindPlayer(
+  int* x1, int* y1, int* z1, int x2, int y2, int z2
+) {
+  float da = *y1;
+  float db = y2;
+  float d = da -db; if (d == 0) { d = 1; }
+  float s = da / (da - db);
+
+  *x1 = *x1 + s * (x2 - (*x1));
+  *y1 = *y1 + s * (y2 - (*y1)); if (*y1 == 0) { *y1 = 1; }
+  *z1 = *z1 + s * (z2 - (*z1)); 
+}
+void drawWall(int x1, int x2, int b1, int b2, int t1, int t2) {
+  int x, y;
+  // hold difference in distance 
+  int dyb = b2 - b1; // y distance of bottom line
+  int dyt = t2-t1;   // y distance of top line
+  int dx  = x2 - x1; // x distance
+  if (dx == 0) { dx = 1; } 
+  int xs = x1;        // hold inital x1 starting position
+
+  // Clip X
+  if (x1 <      1) { x1 = 1; }
+  if (x2 <      1) { x2 = 1; }
+  if (x1 > SW - 1) { x1 = SW - 1; }
+  if (x2 > SW - 1) { x2 = SW - 1; }
+
+  // draw x verticle lines
+  for (x = x1; x < x2; x++) {
+    // the Y start and end point
+    int y1 = dyb * (x - xs + 0.5) / dx + b1;
+    int y2 = dyt * (x - xs + 0.5) / dx + t1;
+
+    // Clip Y
+    if (y1 <      1) { y1 = 1; }
+    if (y2 <      1) { y2 = 1; }
+    if (y1 > SH - 1) { y1 = SH - 1; }
+    if (y2 > SH - 1) { y2 = SH - 1; }
+
+
+    pixel(x, y1, 0); // bottom
+    pixel(x, y2, 0); // bottom
+
+    for (y = y1; y < y2; y++) {
+      pixel(x, y, 0);
+    }
+  }
+
+
+}
 void draw3D() {
   int wx[4], wy[4], wz[4];
   float CS = M.cos[P.a], SN = M.sin[P.a];
@@ -110,27 +160,51 @@ void draw3D() {
   // World X position
   wx[0] = x1 * CS - y1 * SN;
   wx[1] = x2 * CS - y2 * SN;
+  wx[2] = wx[0]; // top line has the same x
+  wx[3] = wx[1];
 
   // World Y position (depth)
   wy[0] = y1 * CS + x1 * SN;
   wy[1] = y2 * CS + x2 * SN;
+  wy[2] = wy[0]; // top line has the same y
+  wy[3] = wy[1];
 
   // World Z position (height)
   wz[0] = 0 - P.z /* next for looks */ + ((P.l * wy[0]) / 32.0);
   wz[1] = 0 - P.z /* next for looks */ + ((P.l * wy[1]) / 32.0);
+  wz[2] = wz[0] + 40;
+  wz[3] = wz[1] + 40;
+
+  // don't draw if behind the player
+  if (wy[0] < 1 && wy[1] < 1) { return; }
+  // point 1 behind the player, clip
+  if (wy[0] < 1) {
+    clipBehindPlayer(&wx[0], &wy[0], &wz[0], wx[1], wy[1], wz[1]); // bottom line
+    clipBehindPlayer(&wx[2], &wy[2], &wz[2], wx[3], wy[3], wz[3]); // top line
+  }
+  // point 2 behind player, clip
+  if (wy[1] < 1) {
+    clipBehindPlayer(&wx[1], &wy[1], &wz[1], wx[0], wy[0], wz[0]); // bottom line
+    clipBehindPlayer(&wx[3], &wy[3], &wz[3], wx[2], wy[2], wz[2]); // top line
+  
+  }
+
 
   // projected screen x, screen y position
   wx[0] = wx[0] * 200 / wy[0] + SW2; wy[0] = wz[0] * 200 / wy[0] + SH2;
   wx[1] = wx[1] * 200 / wy[1] + SW2; wy[1] = wz[1] * 200 / wy[1] + SH2;
+  wx[2] = wx[2] * 200 / wy[2] + SW2; wy[2] = wz[2] * 200 / wy[2] + SH2;
+  wx[1] = wx[3] * 200 / wy[3] + SW2; wy[3] = wz[3] * 200 / wy[3] + SH2;
 
   // draw points
-  if (wx[0] > 0  && wx[0] < SW && wy[0] > 0 && wy[0] < SH) {
-    pixel(wx[0], wy[0], 0);
-  }
+  // if (wx[0] > 0  && wx[0] < SW && wy[0] > 0 && wy[0] < SH) {
+  //   pixel(wx[0], wy[0], 0);
+  // }
 
-  if (wx[1] > 0  && wx[1] < SW && wy[1] > 0 && wy[1] < SH) {
-    pixel(wx[1], wy[1], 0);
-  }
+  // if (wx[1] > 0  && wx[1] < SW && wy[1] > 0 && wy[1] < SH) {
+  //    pixel(wx[1], wy[1], 0);
+  // }
+  drawWall(wx[0], wx[1], wy[0], wy[1], wy[2], wy[3]);
 }
 
 void display() {
