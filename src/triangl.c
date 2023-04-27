ceb1a91c15c1297f85b8b3213940c676d913256c
Luke 10X <spam@luke10x.dev>
2023-04-26 20:13:52 -0400

    index buffer

--- src/triangl.c
+++ src/triangl.c
@@ -144,18 +144,33 @@ int main() {
 	// Vertices coordinates
 	GLfloat vertices[] =
 	{
-		-0.5f, -0.5f * (float)(sqrt(3)) / 3,     0.0f, // Lower left corner
-		 0.5f, -0.5f * (float)(sqrt(3)) / 3,     0.0f, // Lower right corner
-		 0.0f,  0.5f * (float)(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+		-0.5f,    -0.5f * (float)(sqrt(3)) / 3,     0.0f, // Lower left corner
+		0.5f,     -0.5f * (float)(sqrt(3)) / 3,     0.0f, // Lower right corner
+		0.0f,      0.5f * (float)(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+		-0.5f / 2, 0.5f * (float)(sqrt(3)) / 6,     0.0f, // Inner left
+		0.5f / 2,  0.5f * (float)(sqrt(3)) / 6,     0.0f, // Inner right
+		0.0f,     -0.5f * (float)(sqrt(3)) / 3,     0.0f // Inner down
+	};
+
+	// Indices for vertices order
+	GLuint indices[] =
+	{
+		0, 3, 5, // Lower left triangle
+		3, 2, 4, // Lower right triangle
+		5, 4, 1 // Upper triangle
 	};
 
   // Create reference containers for the Vartex Array Object 
   // and the Vertex Buffer Object
-  GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 
-	// Generate the VAO and VBO with only 1 object each
-  glGenVertexArrays(1, &VAO);
+	// Generate the VAO, VBO, and EBO with only 1 object each
+	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	// Make the VAO the current Vertex Array Object by binding it
+	glBindVertexArray(VAO);
 
 	// Bind the VBO specifying it's a GL_ARRAY_BUFFER
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -168,43 +183,41 @@ int main() {
     GL_STATIC_DRAW
   );
 
-  // Make the VAO the current Vertex Array Object by binding it
-	glBindVertexArray(VAO);
+	// Bind the EBO specifying it's a GL_ELEMENT_ARRAY_BUFFER
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	// Introduce the indices into the EBO
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-  int USE_VBO = 1;
-  if (USE_VBO == 1) {
-    // Configure the Vertex Attribute so that OpenGL knows how to read the VBO
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    // // Enable the Vertex Attribute so that OpenGL knows to use it
-    glEnableVertexAttribArray(0);
+  // Configure the Vertex Attribute so that OpenGL knows how to read the VBO
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// Enable the Vertex Attribute so that OpenGL knows to use it
+	glEnableVertexAttribArray(0);
 
-    // Bind both the VBO and VAO to 0 so that we 
-    // don't accidentally modify the VAO and VBO we created
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+	// Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	// Bind the EBO to 0 so that we don't accidentally modify it
+	// MAKE SURE TO UNBIND IT AFTER UNBINDING THE VAO, as the EBO is linked in the VAO
+	// This does not apply to the VBO because the VBO is already linked to the VAO during glVertexAttribPointer
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	// Bind the EBO to 0 so that we don't accidentally modify it
+	// MAKE SURE TO UNBIND IT AFTER UNBINDING THE VAO, as the EBO is linked in the VAO
+	// This does not apply to the VBO because the VBO is already linked to the VAO during glVertexAttribPointer
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-    // Specify the color of the background
-    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-    // Clean the back buffer and assign the new color to it
-    glClear(GL_COLOR_BUFFER_BIT);
-    // Tell OpenGL which Shader Program we want to use
-    glUseProgram(shaderProgram);
-    // Bind the VAO so OpenGL knows to use it
-    glBindVertexArray(VAO);
+  // LOOP can start here //
 
-  } else {
-    glClearColor(0.65f, 0.45f, 0.65f, 1.0f);
-
-    // RENDER
-    glClear(GL_COLOR_BUFFER_BIT);
-    glUseProgram(shaderProgram);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-    glEnableVertexAttribArray(0);
-  }
-  
-  // Draw the triangle using the GL_TRIANGLES primitive
-  glDrawArrays(GL_TRIANGLES, 0, 3);
+  // Specify the color of the background
+  glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+  // Clean the back buffer and assign the new color to it
+  glClear(GL_COLOR_BUFFER_BIT);
+  // Tell OpenGL which Shader Program we want to use
+  glUseProgram(shaderProgram);
+  // Bind the VAO so OpenGL knows to use it
+  glBindVertexArray(VAO);
+  // Draw primitives, number of indices, datatype of indices, index of indices
+  glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
   // Swap the back buffer with the front buffer
   glfwSwapBuffers(window);
   // Take care of all GLFW events
